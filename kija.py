fb25e8de7c1ee59b49f982e8fb47ef5fa30cf1fe
mfahmia <fahsoem@gmail.com>
2017-04-23 23:47:34 +0700

    Update kija.py

--- kija.py
+++ kija.py
@@ -9,35 +9,32 @@ k0=int(binascii.hexlify(k[:4]),16)
 k1=int(binascii.hexlify(k[4:8]),16)
 
 def encrypt(message):
-    hexa=int(binascii.hexlify(message),16)
-    enc=hexa^k0
-    enc=enc+k1
-    enc=enc%pow(2,32)
-    return binascii.unhexlify('%x' %enc)
-
-def decrypt(encrypted):
-    dec=int(binascii.hexlify(encrypted),16)
-    dec-=k1
-    dec%=pow(2,32)
-    dec=dec^k0
-    return binascii.unhexlify('%x' %dec)
-
-def read4char(message,type):
     counter=0
     hasil=''
     while counter<len(message):
-        if(type=='encrypting'):
-            hasil+=encrypt(message[counter:counter+4])
-            counter+=4
-        elif(type=='decrypting'):
-            hasil+=decrypt(message[counter:counter+4])
-            counter+=4
+        hexa=int(binascii.hexlify(message[counter:counter+4]),16)
+        enc=hexa^k0
+        enc=enc+k1
+        enc=enc%pow(2,32)
+        hasil+=binascii.unhexlify('%x' %enc)
+        counter+=4
     return hasil
 
-oaeo=raw_input('String : ')
+def decrypt(encrypted):
+    counter=0
+    hasil=''
+    while counter<len(encrypted):
+        dec=int(binascii.hexlify(encrypted[counter:counter+4]),16)
+        dec-=k1
+        dec%=pow(2,32)
+        dec=dec^k0
+        hasil+=binascii.unhexlify('%x' %dec)
+        counter+=4
+    return hasil
 
-encrypted=read4char(oaeo,'encrypting')
+input=raw_input('String : ')
+
+encrypted=encrypt(input)
 print 'Encrypted : ' + encrypted
-print len(encrypted)
 
-print 'Decrypted : ' + read4char(encrypted,'decrypting')
+print 'Decrypted : ' + decrypt(encrypted)
